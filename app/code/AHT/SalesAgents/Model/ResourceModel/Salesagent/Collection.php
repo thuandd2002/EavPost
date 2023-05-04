2b4d498e855d002e3b0c2396680f74500b81ce28
Dinh Duc Thuan <80194045+thuandd2002@users.noreply.github.com>
2023-05-04 09:20:48 +0700

    sale agents

---
+++ app/code/AHT/SalesAgents/Model/ResourceModel/Salesagent/Collection.php
@@ -0,0 +1,19 @@
+<?php
+namespace AHT\SaleAgents\Model\ResourceModel\Salesagent;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    protected $_idFieldName = 'entity_id';
+    protected $_eventPrefix = 'aht_saleagent_salesagent_collection';
+    protected $_eventObject = 'salesagent_collection';
+
+    /**
+     * Define the resource model & the model.
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('AHT\SalesAgents\Model\Salesagent', 'AHT\SaleAgent\Model\ResourceModel\SalesAgents');
+    }
+}
